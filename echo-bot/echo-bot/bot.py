0c0b512af7819148dd772fc2bd6222bdc2b478fd
=Pavnesh <=2019kuec2037@iiitkota.ac.in>
2020-04-12 12:43:42 +0530

    typing animation

--- echo-bot/echo-bot/bot.py
+++ echo-bot/echo-bot/bot.py
@@ -10,6 +10,26 @@ class MyBot(ActivityHandler):
 
     async def on_message_activity(self, turn_context: TurnContext):
         await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+    
+    # async def on_message_activity(self, turn_context: TurnContext):
+    #     if turn_context.activity.text == "wait":
+    #         return await turn_context.send_activities([
+    #             Activity(
+    #                 type=ActivityTypes.typing
+    #             ),
+    #             Activity(
+    #                 type="delay",
+    #                 value=3000
+    #             ),
+    #             Activity(
+    #                 type=ActivityTypes.message,
+    #                 text="Finished Typing"
+    #             )
+    #         ])
+        else:
+            return await turn_context.send_activity(
+                f"You said {turn_context.activity.text}.  Say 'wait' to watch me type."
+            )
 
     async def on_members_added_activity(
         self,
